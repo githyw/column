5015c10d2ea5354e47258da5686b0315df51a6ec
郝 <961406169@qq.com>
2020-11-19 23:13:53 +0800

    Form components

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# zhihu
+## 项目描述
 
-## Project setup
-```
-npm install
-```
+* 使用Vue3模仿知乎开发的项目
+* 使用bootstrap进行css样式调整
+* 使用TypeScript语法
+* 使用模块化思想开发
+* 代码中含有清晰注释，方便阅读
+* 使用ESLint语法规范
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## 如何运行
 
-### Compiles and minifies for production
-```
-npm run build
-```
+1. git clone https://github.com/githyw/column.git
+2. cd column
+3. npm install
+4. npm run serve
 
-### Lints and fixes files
-```
-npm run lint
-```
+## 在线浏览地址
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+
+>暂时未完成,欢迎下载源码进行查看[]()
+
+## 最后
+`如果对你们有帮助，那么还望顺手点一个star`
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,63 @@
 <template>
   <div class="container">
       <global-header :user="currentUser"></global-header>
+      <validate-form @form-submit="onFormSubmit">
+        <div class="mb-3">
+          <label class="form-label">邮箱地址</label>
+          <validate-input
+          :rules="emailRules"
+          placeholder="请输入邮箱地址"
+          type="text"
+        />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">密码</label>
+          <validate-input
+          type="password"
+          placeholder="请输入密码"
+          />
+        </div>
+        <template #submit>
+          <span class="btn btn-danger">btn</span>
+        </template>
+      </validate-form>
       <coulmn-list :list='list'></coulmn-list>
   </div>
 </template>
-
 <script lang="ts">
 import { defineComponent } from 'vue'
-import CoulmnList, { ColumnlProps } from '@/components/CoulmnList.vue'
-import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
-const currentUser: UserProps = {
-  isLogin: true,
-  name: '郝莹旺'
-}
-const testData: ColumnlProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的tes3专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
+import CoulmnList from '@/components/CoulmnList.vue'
+import testData from '@/hooks/ColumnlProps.ts' // 调用CoulmnList里面定义的接口
+
+import GlobalHeader from '@/components/GlobalHeader.vue'
+import currentUser from '@/hooks/userProps.ts' // 调用GlobalHeader 里面定义的接口
+
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
+import ValidateForm from '@/components/ValidateForm.vue'
+
 export default defineComponent({
   name: 'App',
   components: {
     CoulmnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput,
+    ValidateForm
   },
+  // setup里面可以传入两个参数 第一个参数为props 第二个参数为context
   setup () {
+    // 正则表达式邮箱验证失败时触发的提醒
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const onFormSubmit = (result: boolean) => {
+      console.log(1234, result)
+    }
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRules,
+      onFormSubmit
     }
   }
 })
