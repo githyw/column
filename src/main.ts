87b0f5094479a7f2faf1f800def459f5e1660bbb
郝 <961406169@qq.com>
2021-01-08 01:50:45 +0800

    fail to modify

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ axios.interceptors.request.use(config => {
   // 显示加载时旋转动画
   store.commit('setLoading', true)
   if (config.data instanceof FormData) {
-    config.data.append('icode', '0BC9ABB4AD85B874')
+    config.data.append('icode', 'A604E42F8587A97E')
     store.commit('setLoading', false)
   } else if (config.method === 'post' || config.method === 'patch') {
-    config.data = { ...config.data, icode: '0BC9ABB4AD85B874' }
+    config.data = { ...config.data, icode: 'A604E42F8587A97E' }
   } else {
-    config.params = { ...config.params, icode: '0BC9ABB4AD85B874' }
+    config.params = { ...config.params, icode: 'A604E42F8587A97E' }
   }
   store.commit('setError', { status: false, message: '' })
   return config
